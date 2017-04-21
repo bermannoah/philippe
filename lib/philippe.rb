9ac8a8b02e68e9beda45f30eb3912b67706dd393
Noah Berman <noah@noahberman.org>
2017-04-21 15:16:47 -0500

    almost there

--- lib/philippe.rb
+++ lib/philippe.rb
@@ -1,6 +1,7 @@
 class Philippe
   def self.stand_on_it
-    puts "THIS WILL MODIFY YOUR BASH PROFILE!"
-    system("export alias man='say 'Sorry Philippe is standing on it''")
- end
+    puts "THIS WILL MODIFY YOUR BASH PROFILE! Huuuuuugs!!!"
+    `echo alias man='say "Philippe is standing on it"' >> ~/.bash_profile`
+    puts "Huuuuuuuuugs!!!!!!! ❤️"
+  end
 end
